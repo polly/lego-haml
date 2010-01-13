7f104a4a724bb081f37124ba69a77b75173464b7
Patrik Hedman <patrik@moresale.se>
2010-01-13 13:40:50 +0100

    reorganized files and improved documentation

---
+++ lib/lego/haml.rb
@@ -0,0 +1,52 @@
+module Lego
+  module Haml
+    require 'haml'
+    include ::Haml::Helpers
+    
+    #
+    # Register the plugin with the Lego runtime
+    #
+    def self.register(lego)
+      lego.add_plugin :view, self
+    end
+
+    #
+    # Method for rendering haml input as html.
+    #   <template> can be either a string or a symbol
+    #     - String: Renders the string and returns html
+    #     - Symbol: Looks for a <template>.haml file in
+    #               the path specified by options(:views)
+    #               and renders the contents of that file
+    #
+    #     Example:
+    #       haml "%h1 My Header" #=> "<h1>My Header</h1>\n"
+    #       haml :template       #=> Renders <view_path>/template.haml
+    #
+    #   <locals> is a hash of local variables to pass to 
+    #            the template
+    #
+    #     Example:
+    #        haml "%h1 foo", :foo => "My Header" #=> "<h1>My Header</h1>\n"
+    #
+    # Instance variables are automatically made available to the view
+    #
+    #   Example:
+    #     @foo = "My Template"
+    #     haml "%h1 @foo" #=> "<h1>My Header</h1>\n"
+    #
+    def haml(template, locals={})
+      template = File.read("#{options(:views)}/#{template}.haml") if template.is_a? Symbol
+      ::Haml::Engine.new(template).render(self, locals)
+    end
+
+    private
+      
+      def read_template(template)
+        File.read File.join(options(:views)), "#{template}.haml"
+      end
+
+      def file_not_found_message
+        "No views path specified, add 'set :views => <path>' to you controller"
+      end
+  end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,7 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 require 'rubygems'
-require 'lego-haml'
+require 'lego/haml'
 require 'lego-core'
 require 'rack/test'
 require 'spec'
