e6222399496efeeda99e2197a3cb2ca5892f118c
Patrik Hedman <patrik@moresale.se>
2010-01-13 12:58:11 +0100

    added haml method and specs

--- spec/lego-haml_spec.rb
+++ spec/lego-haml_spec.rb
@@ -1,7 +1,60 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
-describe "LegoHaml" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
+def app
+  Class.new(Lego::Controller) do
+    set :views => File.join(File.dirname(__FILE__), 'views')
+
+    get "/string" do
+      haml "%h1 My Header"
+    end
+
+    get "/instance_variables" do
+      @foo = "foo"
+
+      haml "%h1= @foo"
+    end
+
+    get "/string/with/locals" do
+      haml "%h1= foo", :foo => "foo"
+    end
+
+    get "/file" do
+      haml :template
+    end
+
+    get "/file/with/locals" do
+      haml :file_with_locals, :foo => "foo"
+    end
+  end
+end
+
+describe "Lego Haml" do
+  it "should render a string of haml as html" do
+    get '/string'
+    last_response.body.should eql("<h1>My Header</h1>\n")
+  end
+
+  it "should render .haml files when passed a symbol" do 
+    get '/file'
+
+    last_response.body.should eql("<h1>My Header</h1>\n")
+  end
+
+  it "should have access to instance variables" do
+    get "/instance_variables"
+
+    last_response.body.should eql("<h1>foo</h1>\n")
+  end
+
+  it "should render a string template with locals passed in" do
+    get '/string/with/locals'
+
+    last_response.body.should eql("<h1>foo</h1>\n")
+  end
+
+  it "should render a file template with locals passed in" do
+    get '/file/with/locals'
+
+    last_response.body.should eql("<h1>foo</h1>\n")
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,38 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+require 'rubygems'
 require 'lego-haml'
+require 'lego-core'
+require 'rack/test'
 require 'spec'
 require 'spec/autorun'
 
 Spec::Runner.configure do |config|
-  
+  config.include Rack::Test::Methods 
+end
+
+module Routes
+  def self.register(lego)
+    lego.add_plugin :controller, SinatraController
+    lego.add_plugin :router,     SimpleRouter
+  end
+
+  # implements a basic Sinatra inspiered dsl
+  module SinatraController
+    def get(path, &block)
+      add_route(:get, {:path => path, :action_block => block})
+    end
+  end
+
+  # Implements a very basic mather rule
+  module SimpleRouter
+    def self.match_route(route, env)
+      (route[:path] == env['PATH_INFO']) ? true : false
+    end
+  end
+end
+
+Lego.config do
+  plugin Routes
+  plugin Lego::Haml
 end
